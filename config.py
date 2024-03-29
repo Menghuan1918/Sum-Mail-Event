34ad9613a0e3fffcb0e2346dca4b789bc9ceab52
Menghuan <Menghuan2003@outlook.com>
2024-03-29 13:26:17 +0800

    Add private configuration file and disclaimers

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ import os
 读取配置文件中的信息
 """
 def get_config():
-    with open('config.json', 'r') as f:
-        config = json.load(f)
+    try:
+        with open('config_private.json', 'r') as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        with open('config.json', 'r') as f:
+            config = json.load(f)
     return config
