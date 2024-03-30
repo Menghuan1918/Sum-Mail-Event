0e648fa2ed7333ea710271688684bae3479f41f8
Menghuan <Menghuan2003@outlook.com>
2024-03-30 14:18:51 +0800

    Add file existence check in read_from_file function and remove disclaimers file

--- Tools/text/file_deal.py
+++ Tools/text/file_deal.py
@@ -8,6 +8,8 @@ import re
 
 
 def read_from_file(file_path):
+    if not os.path.exists(file_path):
+        return []
     with open(file_path, "r") as file:
         lines = file.readlines()
     paragraphs = []
