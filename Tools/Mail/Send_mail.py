b5febb9e984afcae1620606083fa5aee59ccd2b6
Menghuan <Menghuan2003@outlook.com>
2024-04-06 11:57:29 +0800

    Add send mail part

---
+++ Tools/Mail/Send_mail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.message import EmailMessage
+from Tools.text.config import get_config
+
+
+def send_mail(Subject, Text):
+    config_data = get_config()
+
+    # 创建邮件对象
+    msg = EmailMessage()
+    msg["From"] = config_data["email_add"]
+    msg["To"] = config_data["send_email"]
+    msg["Subject"] = Subject
+    msg.set_content(Text)
+
+    # 发送邮件
+    with smtplib.SMTP(config_data["smtp_host"], config_data['smtp_port']) as server:
+        server.starttls()
+        server.login(config_data["email_add"], config_data["email_pwd"])
+        server.send_message(msg)
+
+    print("Send mail success!")
