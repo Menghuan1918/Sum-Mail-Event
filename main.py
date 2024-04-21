2105590cf3fa6bca89d7c93998a5490d76614200
Menghuan <Menghuan2003@outlook.com>
2024-04-21 15:36:28 +0800

    Add mail processing logic and send_mail function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Tools.Mail.Get_mail import get_mail
 from Tools.Mail.Deal_mail import deal_the_mail
 from Tools.Mail.Send_mail import send_mail
 import os
+import time
 
 print("\n==========Srart get mail==========")
 get_mail()
@@ -11,6 +12,7 @@ print("\n==========Start deal mail with OCR==========")
 deal("mail")
 print("\n==========Start deal mail with LLM==========")
 # 检查mail文件夹中每个文件夹中是否有sum.txt文件，如果有则不再处理
+# Check if there is a sum.txt file in each folder in the mail folder, if so, no longer process
 for root, dirs, files in os.walk("mail"):
     for dir in dirs:
         if "sum.txt" not in os.listdir(os.path.join(root, dir)):
@@ -20,9 +22,8 @@ for root, dirs, files in os.walk("mail"):
                 print(e)
 
 print("\n==========Start Cheaking the send states==========")
-# 检查mail/weight.txt中所有权重加起来是否超过阀域
-# 其中储存格式为每一行为 权重(数字),'文件夹名'
-
+# 检查mail/weight.txt中所有权重加起来是否超过阀域,其中储存格式为每一行为 权重(数字),'文件夹名'
+# Check whether the sum of all weights in mail/weight.txt exceeds the threshold, where the storage format is that each line is weight (number), 'folder name'
 with open("mail/weight.txt", "r") as f:
     weight = f.readlines()
 config = get_config()
@@ -31,8 +32,8 @@ for line in weight:
     sum += int(line.split(",")[0])
 if sum > config["threshold_value"]:
     print("The weight is over the threshold value, will send the summary of emails.")
-    # 整理邮件内容，分别列出：紧急邮件，优先邮件，普通邮件，骚扰邮件，对应的权重为100,3,2,1
     # 从所在文件夹的sum.txt中读取要发送的内容
+    # Read the content to be sent from the sum.txt in the folder
     urgent = []
     priority = []
     general = []
@@ -45,3 +46,34 @@ if sum > config["threshold_value"]:
                 sum = f.read()
         except:
             sum = mail
+        if weight_temp == "100":
+            urgent.append(sum)
+        elif weight_temp == "3":
+            priority.append(sum)
+        elif weight_temp == "2":
+            general.append(sum)
+        else:
+            harassment.append(sum)
+
+    # 整理邮件内容，分别列出：紧急邮件，优先邮件，普通邮件，骚扰邮件，对应的权重为100,3,2,1
+    # Organize the email content, list the urgent mail, priority mail, general mail, harassment mail respectively, and the corresponding weights are 100,3,2,1
+    mail_content = f"""
+    Urgent mail:
+    {''.join(urgent)}
+    ==========
+    Priority mail:
+    {''.join(priority)}
+    ==========
+    General mail:
+    {''.join(general)}
+    ==========
+    Harassment mail:
+    {''.join(harassment)}
+    """
+    try:
+        os.remove("mail/weight.txt")
+        send_mail(Subject=f"Summary of emails in {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}", Content=mail_content)
+    except Exception as e:
+        print(e)
+
+print("\n==========End==========")
